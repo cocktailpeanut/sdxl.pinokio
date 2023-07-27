c2d1e378b85df4fe124e805f92f5137dd13ec1c3
cocktailpeanut <cocktailpeanuts@proton.me>
2023-07-27 01:02:58 -0400

    update

--- start.js
+++ start.js
@@ -15,7 +15,7 @@ module.exports = async (kernel) => {
         message: "{{os.platform() === 'win32' ? 'webui-user.bat' : 'bash webui.sh -f'}}",
         on: [{
           event: "/(http:\/\/127.0.0.1:[0-9]+)/",
-          return: "{{event.matches[1]}}"
+          return: "{{event.matches[0][1]}}"
         }]
       }
     }, {
